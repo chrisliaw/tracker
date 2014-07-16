d7feac4c30f7d29c4f71fc0574300ad155efec07
chrisliaw <chrisliaw@antrapol.com>
2014-07-16 17:11:04 +0800

    Remove commit object from distributable attributes\nThis table no need to be distributable

--- app/models/commit.rb
+++ app/models/commit.rb
@@ -1,6 +1,6 @@
 class Commit < ActiveRecord::Base
   attr_accessible :committable_id, :committable_type, :identifier, :vcs_affected_files, :vcs_diff, :vcs_info, :vcs_reference
 
-  distributable
+  #distributable
   belongs_to :committable, :polymorphic => true
 end
